fb3fb2b52569ec1033f74a0070ed62c473e82243
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-19 00:43:58 +0300

    Решили 29 задачу

---
+++ №29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите количество элементов Вашего массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] ar = new int [N];
+Console.WriteLine("Введите элементы массива (каждый раз с новой строки): ");
+for (int i = 0; i < N; i++)
+{
+    ar[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Вот элементы Вашего массива в строчку: ");
+for (int i = 0; i < N; i++)
+{
+   Console.Write(ar[i] + " "); 
+}
+
